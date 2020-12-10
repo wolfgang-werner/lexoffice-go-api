816085bdd867787a0d86fce3acc2a947a969992c
Wolfgang Werner <wwerner@picturesafe.de>
2020-12-10 12:19:00 +0100

    added documentation

--- tools.go
+++ tools.go
@@ -2,11 +2,12 @@ package lexoffice
 
 import (
 	"encoding/json"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -14,21 +15,25 @@ const (
 	prettyPrintIndent = "  "
 )
 
+// helper to get a nicely formatted json from an arbitrary struct
 func prettyPrintJson(object interface{}) string {
 	b, _ := json.MarshalIndent(object, prettyPrintPrefix, prettyPrintIndent)
 	return string(b)
 }
 
+// helper to get a dump from an outgoing http request
 func prettyPrintRequest(req *http.Request) string {
 	b, _ := httputil.DumpRequestOut(req, true)
 	return string(b)
 }
 
+// helper to get a dump from a http response
 func prettyPrintResponse(res *http.Response) string {
 	b, _ := httputil.DumpResponse(res, true)
 	return string(b)
 }
 
+// helper to load the LEXOFFICE_API_KEY from environment and/or .env file
 func getApiKey() string {
 	err := godotenv.Load(".env")
 	if err != nil {
