c5cdbdb1e132fc07d17da9423047f16d91247fd7
Wolfgang Werner <wwerner@picturesafe.de>
2020-12-10 12:19:56 +0100

    added company test

--- contacts_test.go
+++ contacts_test.go
@@ -7,54 +7,90 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestClient_CreateContact(t *testing.T) {
+func createCustomerPerson() *Contact {
 	var note = "creator: lexoffice-go-api"
-	var contact = Contact{
-		// read only ID:             nil,
-		// read only OrganizationID: nil,
+	var supplement = "billing address"
+	var street = "Teststraße 123"
+	var zip = "20355"
+	var city = "Hamburg"
+
+	return &Contact{
 		Version: 0,
 		Roles: Roles{
 			Customer: &Customer{},
 		},
-		Company: nil,
-		// Company: nil,
 		Person: &Person{
 			LastName: "Müller",
 		},
 		Addresses: &Addresses{
 			Billing: []Address{
 				{
-					Supplement:  "billing address",
-					Street:      "Teststraße 123",
-					Zip:         "20355",
-					City:        "Hamburg",
+					Supplement:  &supplement,
+					Street:      &street,
+					Zip:         &zip,
+					City:        &city,
 					CountryCode: "DE",
 				},
 			},
-			Shipping: nil,
 		},
 		EmailAddresses: &EmailAddresses{
-			Business: nil,
-			Office:   []string{"wwerner@outlook.com"},
-			Private:  nil,
-			Other:    nil,
+			Office: []string{"wwerner@outlook.com"},
 		},
-		/*
-			PhoneNumbers: &PhoneNumbers{
-				Business: []string{"+49 170 810 92 41"},
-				Office:   nil,
-				Mobile:   nil,
-				Private:  nil,
-				Fax:      nil,
-				Other:    nil,
-			},
-		*/
 		Note:     &note,
 		Archived: false,
 	}
+}
 
+func createCustomerCompany() *Contact {
+	var note = "creator: lexoffice-go-api"
+	var supplement = "billing address"
+	var street = "Teststraße 123"
+	var zip = "20355"
+	var city = "Hamburg"
+
+	return &Contact{
+		Version: 0,
+		Roles: Roles{
+			Customer: &Customer{},
+		},
+		Company: &Company{
+			Name: "ACME Ltd",
+		},
+		Addresses: &Addresses{
+			Billing: []Address{
+				{
+					Supplement:  &supplement,
+					Street:      &street,
+					Zip:         &zip,
+					City:        &city,
+					CountryCode: "DE",
+				},
+			},
+		},
+		EmailAddresses: &EmailAddresses{
+			Office: []string{"wwerner@outlook.com"},
+		},
+		Note:     &note,
+		Archived: false,
+	}
+}
+
+func TestClient_CreateCustomerPerson(t *testing.T) {
 	client := NewClient(getApiKey())
-	contactResponse, err := client.CreateContact(&contact)
+	contactResponse, err := client.CreateContact(createCustomerPerson())
+
+	assert.NoError(t, err, "error calling CreateContact")
+	assert.NotNil(t, contactResponse, "no response")
+
+	if client.debug {
+		// fmt.Printf("%#v\n", contactResponse)
+		fmt.Println(prettyPrintJson(contactResponse))
+	}
+}
+
+func TestClient_CreateCustomerCompany(t *testing.T) {
+	client := NewClient(getApiKey())
+	contactResponse, err := client.CreateContact(createCustomerCompany())
 
 	assert.NoError(t, err, "error calling CreateContact")
 	assert.NotNil(t, contactResponse, "no response")
@@ -67,7 +103,8 @@ func TestClient_CreateContact(t *testing.T) {
 
 func TestGetContact(t *testing.T) {
 	client := NewClient(getApiKey())
-	contact, err := client.GetContact("9919cd4d-0a0c-4fd0-857f-d3963335fc5a")
+	// contact, err := client.GetContact("9919cd4d-0a0c-4fd0-857f-d3963335fc5a")
+	contact, err := client.GetContact("6a5ff2f9-3fe2-4ea1-ae05-a279d28cb55b")
 
 	assert.NoError(t, err, "error calling GetContact")
 	assert.NotNil(t, contact, "no response")
